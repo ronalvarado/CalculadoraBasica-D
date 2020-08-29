0365faa7a02cacafa277da3b1f9edc272dcbb28b
CarolinaOrrego89 <dianacarolinaorrego6gmail.com>
2020-08-28 18:55:41 -0600

    creacion de sensor de proximidad

--- app/src/main/java/diana/orrego/calculadorabasica_d/MainActivity.java
+++ app/src/main/java/diana/orrego/calculadorabasica_d/MainActivity.java
@@ -2,6 +2,11 @@ package diana.orrego.calculadorabasica_d;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
@@ -10,87 +15,66 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    // 1.- Variables
+    // hace la comunicación con el hardware
+    SensorManager sensorManager;
+    // para representar al sensor
+    Sensor sensor;
+    // para determinar si algo de acerca al dispositivo
+    SensorEventListener sensorEventListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Enlace de la variable TextView con la vista
+        final TextView texto = (TextView)findViewById(R.id.tvSensor);
+        // 2.- Aplicando el servicio
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        // El tipo de sensor que se utiliza
+        sensor =
+                sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        // Verificar si el dispositivo tiene este tipo de sensor.
+        // si no lo tiene hayq e terminar la acción
+        if(sensor==null)finish();
+        // llamamos al evento Listener para determinar determinar los cambios
+                sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[0] < sensor.getMaximumRange()){
+
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                    texto.setText("CAMBIANDO A COLOR ROJO");
+                }else{
+
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                    texto.setText("SENSOR DE PROXIMIDAD");
+                }
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        }; // Agregamos el punto y coma
+        // 4.-
+        start();
+    }//End: OnCreate.
+    // 3.-
+    public void start(){
+
+        sensorManager.registerListener(sensorEventListener,sensor,2000*1000);
     }
-    public void calcular(View view) {
-        try {
-            RadioGroup optOperaciones = (RadioGroup) findViewById(R.id.optOperaciones);
-            Spinner cboOperaciones = (Spinner) findViewById(R.id.cboOperaciones);
-
-            TextView tempval = (TextView) findViewById(R.id.txtNum1);
-            double num1 = Double.parseDouble(tempval.getText().toString());
-
-            tempval = (TextView) findViewById(R.id.txtNum2);
-            double num2 = Double.parseDouble(tempval.getText().toString());
-
-            double respuesta = 0;
-            //Este es para el radiogroup y los radiobuttons
-            switch (optOperaciones.getCheckedRadioButtonId()) {
-                case R.id.optSuma:
-                    respuesta = num1 + num2;
-                    break;
-                case R.id.optResta:
-                    respuesta = num1 - num2;
-                    break;
-                case R.id.optMultiplicacion:
-                    respuesta = num1 * num2;
-                    break;
-                case R.id.optDivision:
-                    respuesta = num1 / num2;
-                    break;
-                case R.id.optPorcentaje:
-                    respuesta = (num1 / num2) * num1;
-                    break;
-                case R.id.optExponenciacion:
-                    respuesta = Math.pow(num1, num2);
-                    break;
-                case R.id.optModulo:
-                    respuesta = num1 - (num1 / num2 * num2);
-                    break;
-                case R.id.optFactoreo:
-                    respuesta = num1 / num2;
-                    break;
-
-            }
-            //Este es para el spinner... -> ComboBox.
-            switch (cboOperaciones.getSelectedItemPosition()) {
-                case 1: //suma
-                    respuesta = num1 + num2;
-                    break;
-                case 2: //resta
-                    respuesta = num1 - num2;
-                    break;
-                case 3: //multiplicacion
-                    respuesta = num1 * num2;
-                    break;
-                case 4: //division
-                    respuesta = num1 / num2;
-                    break;
-                case 5: //porcentaje
-                    respuesta = (num1 / num2) * num1;
-                    break;
-                case 6: //exponenciacion
-                    respuesta = Math.pow(num1, num2);
-                    break;
-                case 7: //modulo
-                    respuesta = num1 - (num1 / num2 * num2);
-                    break;
-                case 8: //factoreo
-                    long factorial = 1;
-                    respuesta = (num1 + num2) * factorial;
-                    break;
-            }
-            tempval = (TextView) findViewById(R.id.lblRespuesta);
-            tempval.setText("Respuesta: " + respuesta);
-        } catch (Exception err) {
-            TextView temp = (TextView) findViewById(R.id.lblRespuesta);
-            temp.setText("por favor ingresar los datos correspondientes.");
-
-            Toast.makeText(getApplicationContext(), "por favor ingrese los datos.", Toast.LENGTH_LONG).show();
-        }
+    public void stop(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+    // Estos dos métodos se agregaron haciendo clic derecho en este punto
+// y buscando en la lista de métodos estos
+    @Override
+    protected void onPause() {
+        stop();
+        super.onPause();
+    }
+    @Override
+    protected void onResume() {
+        start();
+        super.onResume();
     }
 }
