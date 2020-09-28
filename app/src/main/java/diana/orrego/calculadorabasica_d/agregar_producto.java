4c47022f6f8a39dfb475aa510e028ee40872c48d
CarolinaOrrego89 <dianacarolinaorrego6gmail.com>
2020-09-27 22:11:38 -0600

    :c

--- app/src/main/java/diana/orrego/calculadorabasica_d/agregar_producto.java
+++ app/src/main/java/diana/orrego/calculadorabasica_d/agregar_producto.java
@@ -29,80 +29,9 @@ public class agregar_producto extends AppCompatActivity {
     String urlCompletaImg;
     Button btnProductos;
     Intent takePictureIntent;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agregar_producto);
-
-        imgFotoProducto = findViewById(R.id.imgFotoProducto);
-
-        btnProductos = findViewById(R.id.btnMostrarProducto);
-        btnProductos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mostrarListaProducto();
-            }
-        });
-        guardarDatosProducto();
-        mostrarDatosProducto();
-        tomarFotoProducto();
-    }
-    void tomarFotoProducto(){
-        imgFotoProducto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    //guardando la imagen
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    }catch (Exception ex){}
-                    if (photoFile != null) {
-                        try {
-                            Uri photoURI = FileProvider.getUriForFile(agregar_producto.this, "com.example.diana.orrego.calculadorabasica_d", photoFile);
-                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                            startActivityForResult(takePictureIntent, 1);
-                        }catch (Exception ex){
-                            Toast.makeText(getApplicationContext(), "Error Toma Foto: "+ ex.getMessage(),Toast.LENGTH_LONG).show();
-                        }
-                    }
-                }
-            }
-        });
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            if (requestCode == 1 && resultCode == RESULT_OK) {
-                Bitmap imageBitmap = BitmapFactory.decodeFile(urlCompletaImg);
-                imgFotoProducto.setImageBitmap(imageBitmap);
-            }
-        }catch (Exception ex){
-            Toast.makeText(getApplicationContext(), "Error: "+ ex.getMessage(), Toast.LENGTH_LONG).show();
-        }
-    }
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "imagen_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        if( storageDir.exists()==false ){
-            storageDir.mkdirs();
-        }
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        // Save a file: path for use with ACTION_VIEW intents
-        urlCompletaImg = image.getAbsolutePath();
-        return image;
-
-    }
-    void guardarDatosProducto(){
         setContentView(R.layout.activity_agregar_producto);
         Button btnGuardarProducto= (Button)findViewById(R.id.btnGuardarProducto);
         btnGuardarProducto.setOnClickListener(new View.OnClickListener() {
